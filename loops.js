02ad3fd79d6568cf7c17feeadab993d713f24fbf
Franky Liu <cuifranky95@gmail.com>
2019-07-27 02:10:23 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop (myArray) {
+  for (let i = 0; i < 25; i++) {
+  if (i === 1) {
+    myArray.push(`I am 1 strange loop.`)
+} else { 
+    myArray.push(`I am ${i} strange loops.`)
+}
+} return myArray
+}
+function whileLoop (aNum) {
+  while (aNum > 0) {
+    console.log(--aNum);
+    if (aNum === 0) {
+      return "done"
+    }
+  }
+}
